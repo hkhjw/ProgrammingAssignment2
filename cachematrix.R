ad17ef058468ff65b9a1ab361daab0e23750082c
hkhjw <hkhjw@hotmail.com>
2022-01-17 18:16:06 +1000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+---
+        title: "R Notebook"
+output: html_notebook
+---
+        
+        ## First function creates the vector that contains a series of functions
+        
+        makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+                set <- function(y){
+                        x <<- y
+                        inv <<- NULL
+                }
+                get <- function() {x}
+                setInverse <- function(inverse) {inv <<- inverse}
+                getInverse <- function () {inv}
+                list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+                
+        }
+## the second function is what calculates the above vector. First by ensuring that it hasnt already been solved
+##then calculating if mean data.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("Getting cached data")
+                return(inv)
+                #this is what returns the already completed calculation if it has already been completed
+                
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
+
+#this took a lot of time because the lessons are actuall pretty awful. Good Luck!
